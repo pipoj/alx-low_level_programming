081f9e3432895760ff0dc0c18684416006dbc36b
Osaigbokan <jeff.osa3400@gmail.com>
2023-07-17 15:38:32 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - no printing with printf and puts
+ *
+ * Return: always (1)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
