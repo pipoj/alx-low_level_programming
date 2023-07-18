fc6098cae0caeeef1f07ba3a6a8f6d22e63482d7
Osaigbokan <jeff.osa3400@gmail.com>
2023-07-17 21:35:03 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - assigning random numbers to variables
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0' ; x < '9' ; x++)
+	{
+		for (x = y + 1 ; y <= '9' ; y++)
+		{
+			if (y != x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x == '8' && y == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
