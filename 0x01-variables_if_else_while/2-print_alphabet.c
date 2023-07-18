000e1d2ecd0a5138be24cb98652397e1a046fd5a
Osaigbokan <jeff.osa3400@gmail.com>
2023-07-17 18:32:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - assigning random numbers to variables
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
