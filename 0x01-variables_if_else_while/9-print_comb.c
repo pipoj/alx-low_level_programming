965646c62864d81d5b73015d2a375f9483af4548
Osaigbokan <jeff.osa3400@gmail.com>
2023-07-17 21:06:25 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,12 @@ int main(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-
-	return (0);
+		putchar('\n');
+		return (0);
 }
