f3e3d5b0de3700e56decf25d8abe20e22457e39b
Osaigbokan <jeff.osa3400@gmail.com>
2023-07-19 20:30:01 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,41 +1,41 @@
 #include "main.h"
 /**
- * print_time_table - printing the times table of the input
- * starting wit 0.
- * @n: the value of the times table to be printed
+ * print_times_table - print time table of n
+ *
+ * @n: takes numner input
  */
+
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int prod, mult, num;
 
-        if (n >= 0 && n <= 15)
-        {
+	if (n <= 15 && n >= 0)
+	{
 		for (num = 0; num <= n; num++)
-                {
-		       	_putchar('0');
-		       	for (mult = 1; mult <= n; mult++)
+		{
+			_putchar(48);
+			for (mult = 1; mult <= n; mult++)
 			{
-                                _putchar('.');
-                                _putchar(' ');
-                                prod = num * mult;
-                                if (prod <= 99)
-                                        _putchar(' ');
-                                                if (prod <= 9)
-                                                        _putchar(' ');
-                                                        {
-                                                                if (pro >= 100)
-                                                                {
-                                                                        _putchar((prod / 100) + '0');
-                                                                        _putchar(((prod / 100)) % 10 + '0');
-                                                                }
-                                                        else if (prod <= 99 && prod >= 10);
-                                                        {
-                                                        _putchar((prod / 100) + '0');
-                                                        }
-                                                        _putchar((prod % 100) + '0');
-                                                        }
-                                                        _putchar('\n');
-                                                        }
-                                                        }
-                                                        }
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				} else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
 }
+
